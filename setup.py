ee8e376ac51c4ddfffd26f8bbd90148921ee2375
Otuodichinma Ugochukwu <davidotuodichinma@gmail.com>
2024-11-03 19:54:11 +0100

    First commit

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/DiaDave405/mypackage',
+    url='https://github.com/DiaDave405/mypackage.git',
     author='Otuodichinma David Ugochukwu',
     author_email='davidotuodichinma@gmail.com'
 )
